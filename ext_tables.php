95ae1d1473e376dddd3eda751faf74e447e674a7
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2013-01-29 08:48:50 +0100

    Workspace preparations (WWW-1301)

--- ext_tables.php
+++ ext_tables.php
@@ -1,48 +1,52 @@
 <?php
-if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
+if (!defined('TYPO3_MODE')) die ('Access denied.');
 
 define('TT_ADDRESS_MAX_IMAGES', 6);
 
-$TCA['tt_address'] = array (
-	'ctrl' => array (
-		'label'             => 'name',
-		'label_alt'         => 'email',
-		'default_sortby'    => 'ORDER BY name',
-		'tstamp'            => 'tstamp',
-		'prependAtCopy'     => 'LLL:EXT:lang/locallang_general.xml:LGL.prependAtCopy',
-		'delete'            => 'deleted',
-		'title'             => 'LLL:EXT:tt_address/locallang_tca.xml:tt_address',
-		'thumbnail'         => 'image',
-		'enablecolumns'     => array (
+$TCA['tt_address'] = array(
+	'ctrl' => array(
+		'label' => 'name',
+		'label_alt' => 'email',
+		'default_sortby' => 'ORDER BY name',
+		'versioningWS' => TRUE,
+		'origUid' => 't3_origuid',
+		'tstamp' => 'tstamp',
+		'prependAtCopy' => 'LLL:EXT:lang/locallang_general.xml:LGL.prependAtCopy',
+		'delete' => 'deleted',
+		'title' => 'LLL:EXT:tt_address/locallang_tca.xml:tt_address',
+		'thumbnail' => 'image',
+		'enablecolumns' => array(
 			'disabled' => 'hidden'
 		),
-		'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY).'tca.php',
-		'iconfile'          => t3lib_extMgm::extRelPath($_EXTKEY).'ext_icon.gif'
+		'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY) . 'tca.php',
+		'iconfile' => t3lib_extMgm::extRelPath($_EXTKEY) . 'ext_icon.gif'
 	),
-	'feInterface' => array (
+	'feInterface' => array(
 		'fe_admin_fieldList' => 'pid,hidden,gender,name,title,address,building,room,birthday,phone,fax,mobile,www,email,city,zip,company,region,country,image,description'
 	)
 );
 
 $TCA['tt_address_group'] = array(
 	'ctrl' => array(
-		'title'                    => 'LLL:EXT:tt_address/locallang_tca.xml:tt_address_group',
-		'label'                    => 'title',
-		'tstamp'                   => 'tstamp',
-		'crdate'                   => 'crdate',
-		'cruser_id'                => 'cruser_id',
-		'sortby'                   => 'title',
-		'delete'                   => 'deleted',
-		'treeParentField'          => 'parent_group',
-		'transOrigPointerField'    => 'l18n_parent',
+		'title' => 'LLL:EXT:tt_address/locallang_tca.xml:tt_address_group',
+		'label' => 'title',
+		'versioningWS' => TRUE,
+		'origUid' => 't3_origuid',
+		'tstamp' => 'tstamp',
+		'crdate' => 'crdate',
+		'cruser_id' => 'cruser_id',
+		'sortby' => 'title',
+		'delete' => 'deleted',
+		'treeParentField' => 'parent_group',
+		'transOrigPointerField' => 'l18n_parent',
 		'transOrigDiffSourceField' => 'l18n_diffsource',
-		'languageField'            => 'sys_language_uid',
-		'enablecolumns'            => array(
+		'languageField' => 'sys_language_uid',
+		'enablecolumns' => array(
 			'disabled' => 'hidden',
 			'fe_group' => 'fe_group',
 		),
-		'dynamicConfigFile'        => t3lib_extMgm::extPath($_EXTKEY).'tca.php',
-		'iconfile'                 => t3lib_extMgm::extRelPath($_EXTKEY).'icon_tt_address_group.gif',
+		'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY) . 'tca.php',
+		'iconfile' => t3lib_extMgm::extRelPath($_EXTKEY) . 'icon_tt_address_group.gif',
 	),
 	'feInterface' => array(
 		'fe_admin_fieldList' => 'hidden, fe_group, title, parent_group, description',
@@ -53,32 +57,32 @@ $TCA['tt_address_group'] = array(
 t3lib_extMgm::addPlugin(
 	array(
 		'LLL:EXT:tt_address/locallang_tca.xml:pi_tt_address',
-		$_EXTKEY.'_pi1'
+		$_EXTKEY . '_pi1'
 	)
 );
 t3lib_extMgm::allowTableOnStandardPages('tt_address');
 t3lib_extMgm::addToInsertRecords('tt_address');
 
-t3lib_extMgm::addLLrefForTCAdescr('tt_address','EXT:tt_address/locallang_csh_ttaddress.xml');
+t3lib_extMgm::addLLrefForTCAdescr('tt_address', 'EXT:tt_address/locallang_csh_ttaddress.xml');
 
 
 // add flexform to pi1
 t3lib_div::loadTCA('tt_content');
-$TCA['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY.'_pi1'] = 'pi_flexform';
-$TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi1'] = 'layout,select_key,pages,recursive';
-t3lib_extMgm::addPiFlexFormValue($_EXTKEY .'_pi1', 'FILE:EXT:tt_address/pi1/flexform.xml');
+$TCA['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY . '_pi1'] = 'pi_flexform';
+$TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY . '_pi1'] = 'layout,select_key,pages,recursive';
+t3lib_extMgm::addPiFlexFormValue($_EXTKEY . '_pi1', 'FILE:EXT:tt_address/pi1/flexform.xml');
 
 t3lib_extMgm::addStaticFile($_EXTKEY, 'static/pi1/', 'Addresses');
 t3lib_extMgm::addStaticFile($_EXTKEY, 'static/old/', 'Addresses (!!!old, only use if you need to!!!)');
 
-if (TYPO3_MODE=='BE') {
-	$TBE_MODULES_EXT['xMOD_db_new_content_el']['addElClasses']['tx_ttaddress_pi1_wizicon'] = t3lib_extMgm::extPath($_EXTKEY).'pi1/class.tx_ttaddress_pi1_wizicon.php';
+if (TYPO3_MODE == 'BE') {
+	$TBE_MODULES_EXT['xMOD_db_new_content_el']['addElClasses']['tx_ttaddress_pi1_wizicon'] = t3lib_extMgm::extPath($_EXTKEY) . 'pi1/class.tx_ttaddress_pi1_wizicon.php';
 
-			// classes for displaying the group tree and manipulating flexforms
-	include_once(t3lib_extMgm::extPath($_EXTKEY).'class.tx_ttaddress_tcefunc_selecttreeview.php');
-	include_once(t3lib_extMgm::extPath($_EXTKEY).'class.tx_ttaddress_treeview.php');
-	include_once(t3lib_extMgm::extPath($_EXTKEY).'class.tx_ttaddress_addfilestosel.php');
-	include_once(t3lib_extMgm::extPath($_EXTKEY).'class.tx_ttaddress_addfieldstosel.php');
+	// classes for displaying the group tree and manipulating flexforms
+	include_once(t3lib_extMgm::extPath($_EXTKEY) . 'class.tx_ttaddress_tcefunc_selecttreeview.php');
+	include_once(t3lib_extMgm::extPath($_EXTKEY) . 'class.tx_ttaddress_treeview.php');
+	include_once(t3lib_extMgm::extPath($_EXTKEY) . 'class.tx_ttaddress_addfilestosel.php');
+	include_once(t3lib_extMgm::extPath($_EXTKEY) . 'class.tx_ttaddress_addfieldstosel.php');
 
 }
 
--- ext_tables.sql
+++ ext_tables.sql
@@ -1,73 +1,91 @@
 #
-# Table structure for table 'tt_address'
+# TABLE structure FOR TABLE 'tt_address'
 #
 CREATE TABLE tt_address (
-  uid int(11) unsigned DEFAULT '0' NOT NULL auto_increment,
-  pid int(11) unsigned DEFAULT '0' NOT NULL,
-  tstamp int(11) unsigned DEFAULT '0' NOT NULL,
-  hidden tinyint(4) unsigned DEFAULT '0' NOT NULL,
-  name tinytext NOT NULL,
-  gender varchar(1) DEFAULT '' NOT NULL,
-  first_name tinytext NOT NULL,
-  middle_name tinytext NOT NULL,
-  last_name tinytext NOT NULL,
-  birthday int(11) DEFAULT '0' NOT NULL
-  title varchar(40) DEFAULT '' NOT NULL,
-  email varchar(80) DEFAULT '' NOT NULL,
-  phone varchar(30) DEFAULT '' NOT NULL,
-  mobile varchar(30) DEFAULT '' NOT NULL,
-  www varchar(80) DEFAULT '' NOT NULL,
-  address tinytext NOT NULL,
-  building varchar(20) DEFAULT '' NOT NULL,
-  room varchar(15) DEFAULT '' NOT NULL,
-  company varchar(80) DEFAULT '' NOT NULL,
-  city varchar(80) DEFAULT '' NOT NULL,
-  zip varchar(20) DEFAULT '' NOT NULL,
-  region varchar(100) DEFAULT '' NOT NULL,
-  country varchar(100) DEFAULT '' NOT NULL,
-  image tinyblob NOT NULL,
-  fax varchar(30) DEFAULT '' NOT NULL,
-  deleted tinyint(3) DEFAULT '0',
-  description text NOT NULL,
-  addressgroup int(11) DEFAULT '0' NOT NULL
-  PRIMARY KEY (uid),
-  KEY parent (pid),
-  KEY pid (pid,email)
+		uid INT(11) unsigned DEFAULT '0' NOT NULL auto_increment,
+		pid INT(11) unsigned DEFAULT '0' NOT NULL,
+		tstamp INT(11) unsigned DEFAULT '0' NOT NULL,
+		hidden tinyint(4) unsigned DEFAULT '0' NOT NULL,
+		NAME tinytext NOT NULL,
+		gender VARCHAR(1) DEFAULT '' NOT NULL,
+		first_name tinytext NOT NULL,
+		middle_name tinytext NOT NULL,
+		last_name tinytext NOT NULL,
+		birthday INT(11) DEFAULT '0' NOT NULL
+		title VARCHAR(40) DEFAULT '' NOT NULL,
+		email VARCHAR(80) DEFAULT '' NOT NULL,
+		phone VARCHAR(30) DEFAULT '' NOT NULL,
+		mobile VARCHAR(30) DEFAULT '' NOT NULL,
+		www VARCHAR(80) DEFAULT '' NOT NULL,
+		address tinytext NOT NULL,
+		building VARCHAR(20) DEFAULT '' NOT NULL,
+		room VARCHAR(15) DEFAULT '' NOT NULL,
+		company VARCHAR(80) DEFAULT '' NOT NULL,
+		city VARCHAR(80) DEFAULT '' NOT NULL,
+		zip VARCHAR(20) DEFAULT '' NOT NULL,
+		region VARCHAR(100) DEFAULT '' NOT NULL,
+		country VARCHAR(100) DEFAULT '' NOT NULL,
+		image tinyblob NOT NULL,
+		fax VARCHAR(30) DEFAULT '' NOT NULL,
+		deleted tinyint(3) DEFAULT '0',
+		description text NOT NULL,
+		addressgroup INT(11) DEFAULT '0' NOT NULL,
+		t3ver_oid INT(11) DEFAULT '0' NOT NULL,
+		t3ver_id INT(11) DEFAULT '0' NOT NULL,
+		t3ver_wsid INT(11) DEFAULT '0' NOT NULL,
+		t3ver_label VARCHAR(30) DEFAULT '' NOT NULL,
+		t3ver_state TINYINT(4) DEFAULT '0' NOT NULL,
+		t3ver_stage TINYINT(4) DEFAULT '0' NOT NULL,
+		t3ver_count INT(11) DEFAULT '0' NOT NULL,
+		t3ver_tstamp INT(11) DEFAULT '0' NOT NULL,
+		t3_origuid INT(11) DEFAULT '0' NOT NULL
+		PRIMARY KEY (uid),
+		KEY parent (pid),
+		KEY pid (pid,email)
 );
 
 
 #
-# Table structure for table 'tx_addressgroups_group'
+# TABLE structure FOR TABLE 'tx_addressgroups_group'
 #
 CREATE TABLE tt_address_group (
-	uid int(11) NOT NULL auto_increment,
-	pid int(11) DEFAULT '0' NOT NULL,
-	tstamp int(11) DEFAULT '0' NOT NULL,
-	crdate int(11) DEFAULT '0' NOT NULL,
-	cruser_id int(11) DEFAULT '0' NOT NULL,
-	sorting int(10) DEFAULT '0' NOT NULL,
-	deleted tinyint(4) DEFAULT '0' NOT NULL,
-	hidden tinyint(4) DEFAULT '0' NOT NULL,
-	fe_group int(11) DEFAULT '0' NOT NULL,
-	title tinytext NOT NULL,
-	parent_group int(11) DEFAULT '0' NOT NULL,
-	description text NOT NULL,
-	sys_language_uid int(11) DEFAULT '0' NOT NULL,
-	l18n_parent int(11) DEFAULT '0' NOT NULL,
-	l18n_diffsource mediumblob NOT NULL,
-	PRIMARY KEY (uid),
-	KEY parent (pid)
+		uid INT(11) NOT NULL auto_increment,
+		pid INT(11) DEFAULT '0' NOT NULL,
+		tstamp INT(11) DEFAULT '0' NOT NULL,
+		crdate INT(11) DEFAULT '0' NOT NULL,
+		cruser_id INT(11) DEFAULT '0' NOT NULL,
+		sorting INT(10) DEFAULT '0' NOT NULL,
+		deleted tinyint(4) DEFAULT '0' NOT NULL,
+		hidden tinyint(4) DEFAULT '0' NOT NULL,
+		fe_group INT(11) DEFAULT '0' NOT NULL,
+		title tinytext NOT NULL,
+		parent_group INT(11) DEFAULT '0' NOT NULL,
+		description text NOT NULL,
+		sys_language_uid INT(11) DEFAULT '0' NOT NULL,
+		l18n_parent INT(11) DEFAULT '0' NOT NULL,
+		l18n_diffsource mediumblob NOT NULL,
+		t3ver_oid INT(11) DEFAULT '0' NOT NULL,
+		t3ver_id INT(11) DEFAULT '0' NOT NULL,
+		t3ver_wsid INT(11) DEFAULT '0' NOT NULL,
+		t3ver_label VARCHAR(30) DEFAULT '' NOT NULL,
+		t3ver_state TINYINT(4) DEFAULT '0' NOT NULL,
+		t3ver_stage TINYINT(4) DEFAULT '0' NOT NULL,
+		t3ver_count INT(11) DEFAULT '0' NOT NULL,
+		t3ver_tstamp INT(11) DEFAULT '0' NOT NULL,
+		t3_origuid INT(11) DEFAULT '0' NOT NULL
+		PRIMARY KEY (uid),
+		KEY parent (pid)
 );
 
 #
-# Table structure for table 'tt_address_tx_addressgroups_group_mm'
+# TABLE structure FOR TABLE 'tt_address_tx_addressgroups_group_mm'
 #
 #
 CREATE TABLE tt_address_group_mm (
-  uid_local int(11) DEFAULT '0' NOT NULL,
-  uid_foreign int(11) DEFAULT '0' NOT NULL,
-  tablenames varchar(30) DEFAULT '' NOT NULL,
-  sorting int(11) DEFAULT '0' NOT NULL,
-  KEY uid_local (uid_local),
-  KEY uid_foreign (uid_foreign)
+		uid_local INT(11) DEFAULT '0' NOT NULL,
+		uid_foreign INT(11) DEFAULT '0' NOT NULL,
+		tablenames VARCHAR(30) DEFAULT '' NOT NULL,
+		sorting INT(11) DEFAULT '0' NOT NULL,
+		KEY uid_local (uid_local),
+		KEY uid_foreign (uid_foreign)
 );
