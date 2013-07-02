17751898f46275b457a066ca54aafd2a3e798258
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2013-07-02 08:57:10 +0200

    Style of ext_tables.sql

--- ext_tables.sql
+++ ext_tables.sql
@@ -2,37 +2,37 @@
 # Table structure for table 'tt_address'
 #
 CREATE TABLE tt_address (
-  uid int(11) unsigned NOT NULL auto_increment,
-  pid int(11) unsigned DEFAULT '0' NOT NULL,
-  tstamp int(11) unsigned DEFAULT '0' NOT NULL,
-  hidden tinyint(4) unsigned DEFAULT '0' NOT NULL,
-  name tinytext NOT NULL,
-  gender varchar(1) DEFAULT '' NOT NULL,
-  first_name tinytext NOT NULL,
-  middle_name tinytext NOT NULL,
-  last_name tinytext NOT NULL,
-  birthday int(11) DEFAULT '0' NOT NULL,
-  title varchar(255) DEFAULT '' NOT NULL,
-  email varchar(255) DEFAULT '' NOT NULL,
-  phone varchar(30) DEFAULT '' NOT NULL,
-  mobile varchar(30) DEFAULT '' NOT NULL,
-  www varchar(255) DEFAULT '' NOT NULL,
-  address tinytext NOT NULL,
-  building varchar(20) DEFAULT '' NOT NULL,
-  room varchar(15) DEFAULT '' NOT NULL,
-  company varchar(255) DEFAULT '' NOT NULL,
-  city varchar(255) DEFAULT '' NOT NULL,
-  zip varchar(20) DEFAULT '' NOT NULL,
-  region varchar(255) DEFAULT '' NOT NULL,
-  country varchar(128) DEFAULT '' NOT NULL,
-  image tinyblob NOT NULL,
-  fax varchar(30) DEFAULT '' NOT NULL,
-  deleted tinyint(3) DEFAULT '0',
-  description text NOT NULL,
-  addressgroup int(11) DEFAULT '0' NOT NULL,
-  PRIMARY KEY (uid),
-  KEY parent (pid),
-  KEY pid (pid,email)
+		uid INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+		pid INT(11) UNSIGNED DEFAULT '0' NOT NULL,
+		tstamp INT(11) UNSIGNED DEFAULT '0' NOT NULL,
+		hidden TINYINT(4) UNSIGNED DEFAULT '0' NOT NULL,
+		name TINYTEXT NOT NULL,
+		gender VARCHAR(1) DEFAULT '' NOT NULL,
+		first_name TINYTEXT NOT NULL,
+		middle_name TINYTEXT NOT NULL,
+		last_name TINYTEXT NOT NULL,
+		birthday INT(11) DEFAULT '0' NOT NULL,
+		title VARCHAR(255) DEFAULT '' NOT NULL,
+		email VARCHAR(255) DEFAULT '' NOT NULL,
+		phone VARCHAR(30) DEFAULT '' NOT NULL,
+		mobile VARCHAR(30) DEFAULT '' NOT NULL,
+		www VARCHAR(255) DEFAULT '' NOT NULL,
+		address TINYTEXT NOT NULL,
+		building VARCHAR(20) DEFAULT '' NOT NULL,
+		room VARCHAR(15) DEFAULT '' NOT NULL,
+		company VARCHAR(255) DEFAULT '' NOT NULL,
+		city VARCHAR(255) DEFAULT '' NOT NULL,
+		zip VARCHAR(20) DEFAULT '' NOT NULL,
+		region VARCHAR(255) DEFAULT '' NOT NULL,
+		country VARCHAR(128) DEFAULT '' NOT NULL,
+		image TINYBLOB NOT NULL,
+		fax VARCHAR(30) DEFAULT '' NOT NULL,
+		deleted TINYINT(3) DEFAULT '0',
+		description TEXT NOT NULL,
+		addressgroup INT(11) DEFAULT '0' NOT NULL,
+		PRIMARY KEY (uid),
+		KEY parent (pid),
+		KEY pid (pid, email)
 );
 
 
@@ -40,23 +40,23 @@ CREATE TABLE tt_address (
 # Table structure for table 'tx_addressgroups_group'
 #
 CREATE TABLE tt_address_group (
-	uid int(11) NOT NULL auto_increment,
-	pid int(11) DEFAULT '0' NOT NULL,
-	tstamp int(11) DEFAULT '0' NOT NULL,
-	crdate int(11) DEFAULT '0' NOT NULL,
-	cruser_id int(11) DEFAULT '0' NOT NULL,
-	sorting int(10) DEFAULT '0' NOT NULL,
-	deleted tinyint(4) DEFAULT '0' NOT NULL,
-	hidden tinyint(4) DEFAULT '0' NOT NULL,
-	fe_group int(11) DEFAULT '0' NOT NULL,
-	title tinytext NOT NULL,
-	parent_group int(11) DEFAULT '0' NOT NULL,
-	description text NOT NULL,
-	sys_language_uid int(11) DEFAULT '0' NOT NULL,
-	l18n_parent int(11) DEFAULT '0' NOT NULL,
-	l18n_diffsource mediumblob NOT NULL,
-	PRIMARY KEY (uid),
-	KEY parent (pid)
+		uid INT(11) NOT NULL AUTO_INCREMENT,
+		pid INT(11) DEFAULT '0' NOT NULL,
+		tstamp INT(11) DEFAULT '0' NOT NULL,
+		crdate INT(11) DEFAULT '0' NOT NULL,
+		cruser_id INT(11) DEFAULT '0' NOT NULL,
+		sorting INT(10) DEFAULT '0' NOT NULL,
+		deleted TINYINT(4) DEFAULT '0' NOT NULL,
+		hidden TINYINT(4) DEFAULT '0' NOT NULL,
+		fe_group INT(11) DEFAULT '0' NOT NULL,
+		title TINYTEXT NOT NULL,
+		parent_group INT(11) DEFAULT '0' NOT NULL,
+		description TEXT NOT NULL,
+		sys_language_uid INT(11) DEFAULT '0' NOT NULL,
+		l18n_parent INT(11) DEFAULT '0' NOT NULL,
+		l18n_diffsource MEDIUMBLOB NOT NULL,
+		PRIMARY KEY (uid),
+		KEY parent (pid)
 );
 
 #
@@ -64,10 +64,10 @@ CREATE TABLE tt_address_group (
 #
 #
 CREATE TABLE tt_address_group_mm (
-  uid_local int(11) DEFAULT '0' NOT NULL,
-  uid_foreign int(11) DEFAULT '0' NOT NULL,
-  tablenames varchar(30) DEFAULT '' NOT NULL,
-  sorting int(11) DEFAULT '0' NOT NULL,
-  KEY uid_local (uid_local),
-  KEY uid_foreign (uid_foreign)
+		uid_local INT(11) DEFAULT '0' NOT NULL,
+		uid_foreign INT(11) DEFAULT '0' NOT NULL,
+		tablenames VARCHAR(30) DEFAULT '' NOT NULL,
+		sorting INT(11) DEFAULT '0' NOT NULL,
+		KEY uid_local (uid_local),
+		KEY uid_foreign (uid_foreign)
 );
